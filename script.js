63f5e7eb8d07569cf47e27506b98b55857d729c6
jayantsingh16 <jayant.singh@nl.abnamro.com>
2021-06-15 13:42:48 +0200

    2 files modified, 1 added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello!')
+function readFile(file) {
+    var reader = new FileReader();
+    reader.onload = function (evt) {
+        var textContents = evt.target.result;
+        console.log(textContents);
+    };
+    reader.readAsText(file);
+}
+
+var readFileBtn = document.getElementById('xml1');
+readFileBtn.addEventListener('click', function () {
+    readFile('users.txt');
+});
